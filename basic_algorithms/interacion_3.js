dcf90617a2c6d466f704bc7b3cb769fb92815795
i-am-iron-man <raquel.angona@bootcamp-upgrade.com>
2022-05-11 20:41:35 +0200

    Primer envío

---
+++ basic_algorithms/interacion_3.js
@@ -0,0 +1,12 @@
+/*1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+1.4 Usa el correcto operador de asignación que resultará en x = 15, 
+teniendo dos variables y = 10 y z = 5.
+
+1.5 Usa el correcto operador de asignación que resultará en x = 50,
+teniendo dos variables y = 10 y z = 5.
+*/
