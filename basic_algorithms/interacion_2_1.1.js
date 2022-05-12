aefa63c3f18f13c8e54b43a940974b7c03890f94
i-am-iron-man <raquel.angona@bootcamp-upgrade.com>
2022-05-12 08:51:17 +0200

    actualización de ejercicios

---
+++ basic_algorithms/interacion_2_1.1.js
@@ -0,0 +1,5 @@
+/*
+1.1 Dado el siguiente javascript, cambia el valor de la propiedad age a 25.*/
+const character = {name: 'Jack Sparrow', age: 10};
+age = 25;
+console.log(character);
---
+++ basic_algorithms/iteracion_2_1.2.js
@@ -0,0 +1,13 @@
+/*1.2 Declara 3 variables con los nombres y valores siguientes 
+	firstName = 'Jon'; 
+	lastName = 'Snow'; 
+	age = 24; 
+Muestralos por consola de esta forma: 
+	'Soy Jon Snow, tengo 24 años y me gustan los lobos.'*/
+
+    const firstName = "Jon";
+    const lastName = "Snow";
+    const age = 24;
+    
+    console.log("Soy " + firstName + " " +lastName +", tengo " + age +" años y me gustan los lobos.");
+
